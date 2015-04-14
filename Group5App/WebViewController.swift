59a22fe616561b436e4a424ae2722f24e7ab8bb5
ama7w2 <ama7w2@mail.missouri.edu>
2015-04-14 15:55:15 -0500

    Implemented parser and NSURLSession
    yep.

--- Group5App/WebViewController.swift
+++ Group5App/WebViewController.swift
@@ -10,26 +10,21 @@ import UIKit
 
 class WebViewController: UIViewController {
 
+    var article:Article?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navBar.title = article!.section
+        
+        let requestString = article!.url
+        
+        if requestString != nil{
+            let requestURL = NSURL(string: requestString!)
+            let request = NSURLRequest(URL: requestURL!)
+            webViewer.loadRequest(request)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
